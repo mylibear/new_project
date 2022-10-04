529ff0a9768e0e1db7fdfa5845b8501a82de794d
mylibear <90846053+mylibear@users.noreply.github.com>
2022-10-04 20:11:35 +1000

    add

--- modified/server/index.js
+++ modified/server/index.js
@@ -42,10 +42,10 @@ io.on("connection", (socket) => {
     onlineUsers.set(userId, socket.id);
   });
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-    }
-  });
+  // socket.on("send-msg", (data) => {
+  //   const sendUserSocket = onlineUsers.get(data.to);
+  //   if (sendUserSocket) {
+  //     socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+  //   }
+  // });
 });
